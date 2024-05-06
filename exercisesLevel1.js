4d9ff93159a617b96ff7e233b27fcedb8e2acced
renan <renanqueiroz96@gmail.com>
2024-05-05 21:10:01 -0400

    Exercise from 1 to 12

---
+++ exercisesLevel1.js
@@ -0,0 +1,13 @@
+let challenge = '30 Days of JavaScript';
+console.log(challenge);
+console.log('Length: '+challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(11,15)) //Java
+console.log(challenge.substring(3,30)) //Days of JavaScript
+console.log(challenge.includes('Script')) //True
+console.log(challenge.split()) //changes to an array
+console.log(challenge.split(' ')) //split to an Array(5) [ "Var:", "30", "Days", "of", "JavaScript" ]
+var companies = 'Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon'
+console.log(companies.split(','))
+console.log(challenge.replace('JavaScript', 'Python'))
